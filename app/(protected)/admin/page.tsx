1309d45001915735b460d66176266a6689598300
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-01-16 18:22:00 +0100

    test

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -8,26 +8,27 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
 import { toast } from "sonner";
 
-export const AdminPage = () => {
-    const onApiRouteClick = () => {
-        fetch("/api/admin")
-            .then((response) => {
-                if (response.ok) {
-                    toast.success("Allowed API Route!")
-                } else {
-                    toast.error("Forbidden API Route!")
-                }
-            })
-    }
-
+const AdminPage = () => {
     const onServerActionClick = () => {
         admin()
             .then((data) => {
                 if (data.error) {
-                    toast.error(data.error)
+                    toast.error(data.error);
                 }
+
                 if (data.success) {
-                    toast.success(data.success)
+                    toast.success(data.success);
+                }
+            })
+    }
+
+    const onApiRouteClick = () => {
+        fetch("/api/admin")
+            .then((response) => {
+                if (response.ok) {
+                    toast.success("Allowed API Route!");
+                } else {
+                    toast.error("Forbidden API Route!");
                 }
             })
     }
@@ -40,17 +41,24 @@ export const AdminPage = () => {
                 </p>
             </CardHeader>
             <CardContent className="space-y-4">
-                <RoleGate allowedRole={UserRole.ADMIN} >
-                    <FormSuccess message="You are allowed!" />
+                <RoleGate allowedRole={UserRole.ADMIN}>
+                    <FormSuccess
+                        message="You are allowed to see this content!"
+                    />
                 </RoleGate>
-                <div className="flex flex-row items-center justify-between rounded-md border p-3 font-medium">
-                    <p className="text-sm font-medium">Admin-only API Route</p>
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">
+                        Admin-only API Route
+                    </p>
                     <Button onClick={onApiRouteClick}>
                         Click to test
                     </Button>
                 </div>
-                <div className="flex flex-row items-center justify-between rounded-md border p-3 font-medium">
-                    <p className="text-sm font-medium">Admin-only server action</p>
+
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">
+                        Admin-only Server Action
+                    </p>
                     <Button onClick={onServerActionClick}>
                         Click to test
                     </Button>
