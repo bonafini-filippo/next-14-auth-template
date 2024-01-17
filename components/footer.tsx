773b55f37fe5abb517a560c74e7603965f1c11f9
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-01-17 19:49:04 +0100

    remove logo to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
     return (
         <footer className="p-4 bg-zinc-100 md:p-8 lg:p-10 dark:bg-gray-800">
             <div className="mx-auto max-w-screen-xl text-center">
-                <Link href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
+                {/*  <Link href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
                     <Image width={100} height={100} alt="Logo-Fb-Development" src="/logo-black.svg" />
-                </Link>
+                </Link> */}
                 <p className="my-6 text-gray-500 dark:text-gray-400">Il sito web perfetto per la tua attività</p>
                 <nav>
                     <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
