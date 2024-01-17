b46e9c3cd96120f72b41394c3d50f8b3baad74c6
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-01-17 21:51:22 +0100

    a

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-
-        <div className="grid lg:grid-cols-2 container md:mt-20 my-3 gap-10 md:gap-20" >
-            {children}
+        <section className="lg:grid lg:grid-cols-2 container my-3 lg:my-28 gap-10 md:gap-20 h-full" >
+            <div>
+                {children}
+            </div>
             <div className="lg:flex justify-center items-center hidden">
                 <Image src="/login.png" width={400} height={400} alt="login-form-image" quality={100} />
             </div>
-        </div>
-
-
+        </section>
     );
 };
 
