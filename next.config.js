2a3f693889521b11d1f6b60753a366d16902fcc7
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-01-19 19:11:38 +0100

    a

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ['en', 'it', 'de'],
-        defaultLocale: 'de',
-    },
+    /*     i18n: {
+            locales: ['en', 'it', 'de'],
+            defaultLocale: 'de',
+        }, */
 }
 
 module.exports = nextConfig
