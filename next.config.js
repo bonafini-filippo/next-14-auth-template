d38085568463a508c1228063962c1c83a2d5b1a7
bonafini-filippo <bonafiniferrarifilippo@gmail.com>
2024-01-19 18:47:30 +0100

    default lang

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n: {
-        locales: ['en-US', 'de', 'it'],
-        defaultLocale: 'de',
+        locales: ['en-US', 'it'],
+        defaultLocale: 'it',
     },
 }
 
